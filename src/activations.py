44f89dd594e59a1d417cf60e9fb04c42b2ae566c
Corina Catarau <corinacatarau@corinas-mbp.home>
2019-11-19 00:34:33 +0000

    Batch run

--- src/activations.py
+++ src/activations.py
@@ -18,19 +18,7 @@ class ActivationFunction:
     def __init__(self, x_arr=None, name=CHOICES[0], layer=None):
         self.function = getattr(self, name, None)
         if name == 'softmax':
-            x_flatten = flatten(x_arr)
-            stable_softmax = self.softmax_stabilizer(x_flatten)
-            x_arr_var = [auto_diff.Var(name=f'x_{i}', value=float(stable_softmax[i])) for i in range(len(stable_softmax))]
-            exp_arr = [auto_diff.Exponential(var) for var in x_arr_var]
-            sum_denominator = reduce(lambda x, y: auto_diff.Add(x, y), exp_arr)
-            softmax_results = [self.function(x, sum_denominator, activation) for (x, activation) in zip(exp_arr, x_arr_var)]
-            softmax_forward = [softmax_result[0] for softmax_result in softmax_results]
-            softmax_back = [softmax_result[1] for softmax_result in softmax_results]
-            self.output,  = np.array(
-                softmax_forward,
-                ndmin=2
-            ).reshape(x_arr.shape)
-            layer.derivative = softmax_back
+            self.softmax_batch(x_arr, layer)
         else:
             self.derivative = None
             if x_arr is not None:
@@ -41,6 +29,36 @@ class ActivationFunction:
                 ).reshape(x_arr.shape)
                 layer.derivative = self.gradient
         return
+
+    def softmax_batch(self, x_arr, layer):
+        nr_values = x_arr.shape[0]
+        if nr_values == 1:
+            softmax_sample = self.softmax_sample(x_arr)
+            output = np.array(
+                softmax_sample,
+                ndmin=2
+            ).reshape(x_arr.shape)
+            self.output = output
+            layer.derivative = np.array(softmax_sample['deriv']).reshape(x_arr.shape)
+        else:
+            lambda_soft = [self.softmax_sample(np.array(x, ndmin=2)) for x in x_arr]
+            lambda_result = pd.DataFrame(lambda_soft)
+            self.output = np.array(lambda_result['out'].to_list()).reshape(x_arr.shape)
+            layer.derivative = np.array(lambda_result['deriv'].to_list()).reshape(x_arr.shape)
+
+        return
+
+    def softmax_sample(self, x_arr):
+        x_flatten = flatten(x_arr)
+        stable_softmax = self.softmax_stabilizer(x_flatten)
+        x_arr_var = [auto_diff.Var(name=f'x_{i}', value=float(stable_softmax[i])) for i in range(len(stable_softmax))]
+        exp_arr = [auto_diff.Exponential(var) for var in x_arr_var]
+        sum_denominator = reduce(lambda x, y: auto_diff.Add(x, y), exp_arr)
+        softmax_results = [self.function(x, sum_denominator, activation) for (x, activation) in zip(exp_arr, x_arr_var)]
+        softmax_forward = [softmax_result[0] for softmax_result in softmax_results]
+        softmax_back = [softmax_result[1] for softmax_result in softmax_results]
+
+        return {'out': softmax_forward, 'deriv': softmax_back}
 
     def softmax_stabilizer(self, x_arr):
         max_x_arr = np.max(x_arr)
@@ -89,6 +107,19 @@ class CostFunction:
         return 1/2*np.sum((target-predicted)**2)
 
     def categorical_cross_entropy(self, predicted, target):
+        shape = target.shape[0]
+        if shape == 1:
+            cost = self.categorical_cross_entropy_sample(predicted, target)
+            self.derivative_cost = cost['derivative_error']
+            return cost['error']
+        else:
+            lambda_ce = [self.categorical_cross_entropy_sample(predicted[i], target[i]) for i in range(len(target))]
+            lambda_result = pd.DataFrame(lambda_ce)
+            output = (np.array(lambda_result['error'].to_list()).sum())/len(target)
+            self.derivative_cost = np.mean(np.array(lambda_result['derivative_error'].to_list()), axis=0, keepdims=True)
+            return output
+
+    def categorical_cross_entropy_sample(self, predicted, target):
         target_flatten = flatten(target)
         predicted_flatten = flatten(predicted)
         target_var = [auto_diff.Var(name=f't_{i}', value=float(target_flatten[i]))
@@ -99,8 +130,8 @@ class CostFunction:
         prod_target_predicted = [auto_diff.Mul(ti, log_pred_i) for ti, log_pred_i in zip(target_var, log_predicted_var)]
         sum_ce = reduce(lambda x, y: auto_diff.Add(x, y), prod_target_predicted)
         sum_ce = auto_diff.Mul(auto_diff.Constant(-1), sum_ce)
-        self.derivative_cost = np.array([sum_ce.gradient(pred_var).eval() for pred_var in predicted_var]).reshape(
+        cost = np.array([sum_ce.gradient(pred_var).eval() for pred_var in predicted_var]).reshape(
             predicted.shape
         )
 
-        return sum_ce.eval()
+        return {'error': sum_ce.eval(), 'derivative_error': cost}
